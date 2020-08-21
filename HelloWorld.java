659f28eaae48830d055be16fbc0cf1a1d231cc23
Minjae Kim <22minjae.kim@boiseschools.net>
2020-08-21 16:37:40 +0000

    finished!

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+//Minjae kIm
+//AP computer sceience
+//2020-08-21
+
+
+
+public class HelloWorld{ 
+  public static void main(String args[]){
+    System.out.println("$$$_____$$$$$$$_$$$$$$$_$$$_______$$$_$$$$$$$$$$");
+    System.out.println("$$$____$$$____$$$____$$$_$$$_____$$$__$$$_______");
+    System.out.println("$$$____$$$_____$_____$$$_$$$_____$$$__$$$_______");
+    System.out.println("$$$_____$$$_________$$$___$$$___$$$___$$$$$$$$__");
+    System.out.println("$$$______$$$_______$$$_____$$$_$$$____$$$_______");
+    System.out.println("$$$_______$$$_____$$$______$$$_$$$____$$$_______");
+    System.out.println("$$$$$$$$$___$$$$$$$_________$$$$$_____$$$$$$$$$$");
+  }
+}
